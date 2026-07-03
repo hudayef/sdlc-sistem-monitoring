c9da7d02c75c6532a3bf5f47176d55de79e2c4b0
Muhamad Kaisar Hudayef <kaisarhudayef1@gmail.com>
2026-07-03 15:17:27 +0700

    fix: change base path to relative to support vercel and github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sdlc-sistem-monitoring/',
+  base: './',
   plugins: [...react()],
 })
